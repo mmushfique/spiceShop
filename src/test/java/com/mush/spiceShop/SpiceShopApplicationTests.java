5dc2e3dae232fde58a3406dfc37073f9725e5e4d
mushfiqueCus <mushfique@custella.com>
2023-08-18 13:27:31 +0800

    initial commit

---
+++ src/test/java/com/mush/spiceShop/SpiceShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mush.spiceShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiceShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
