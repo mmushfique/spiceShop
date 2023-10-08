ff4f143dc82521ca4b848e440ebe4673b150cedf
mushfiqueCus <mushfique@custella.com>
2023-10-08 19:40:21 +0800

    modified html files

--- src/main/java/com/mush/spiceShop/dto/ProductOutputDTO.java
+++ src/main/java/com/mush/spiceShop/dto/ProductOutputDTO.java
@@ -11,14 +11,18 @@ public class ProductOutputDTO implements Serializable {
     String description;
     Double rateFrom0;
     Double rateFrom50;
+    String category;
     String unit;
+    String imageUrl;
     public ProductOutputDTO(Product product) {
         this.id = product.getId();
         this.name = product.getName();
         this.description = product.getDescription();
         this.rateFrom0 = product.getRateFrom0();
         this.rateFrom50 = product.getRateFrom50();
+        this.category=product.getCategory();
         this.unit = product.getUnit();
+        this.imageUrl= product.getImageUrl();
     }
 
 }
